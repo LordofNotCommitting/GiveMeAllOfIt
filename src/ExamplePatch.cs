e0a17657be9a981f1c6a80ccb4707423a2bf3e16
LordofNotCommitting <shadymcshade247@gmail.com>
2025-12-08 21:41:13 -0500

    1.0

---
+++ src/ExamplePatch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using MGSC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace GiveMeAllOfIt
+{
+    [HarmonyPatch(typeof(StationTradeItemPanel), nameof(StationTradeItemPanel.IncreaseButtonOnClick))]
+    public static class ExamplePatch
+    {
+        public static void Prefix(StationTradeItemPanel __instance, CommonButton arg1, int arg2)
+        {
+            //Plugin.Logger.Log("--- main menu awake");
+
+            __instance.TradeCount = Mathf.Clamp(__instance.TradeCount + 10000, 0, __instance.ItemsCount);
+        }
+
+    }
+}
